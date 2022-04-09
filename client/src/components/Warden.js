db95f211b045e876357f742ed84a900c5fdff685
Aman Verma <itz.aman.av@gmail.com>
2022-04-09 18:15:48 +0530

    Conflicts removed

--- client/src/components/Warden.js
+++ client/src/components/Warden.js
@@ -17,7 +17,7 @@ function Warden() {
         e.preventDefault();
         console.log({username,date})
         const res = await axios.get('/hat', {
-          params: {
+          headers: {
             username: username,
             datee:date
             //   date:date
